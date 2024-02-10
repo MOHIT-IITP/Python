7b65f7290c63eef7179a2c64d781f7b099a4534c
MOHIT-IITP <codingbymohit@gmail.com>
2024-02-10 17:36:43 +0000

    Add inverted pyramid

---
+++ CODE/pattern/inverted_pyramid.py
@@ -0,0 +1,19 @@
+# the output will be something like this 
+# **********
+# *********
+# ********
+# *******
+# ******
+# *****
+# ****
+# ***
+# **
+# *
+
+def pattern(row):
+    for i in range(row,0,-1):
+        for j in range(i,0,-1):
+            print("*",end="")
+        print("\n")
+row=int(input("Enter the number of row: "))
+pattern(row)
