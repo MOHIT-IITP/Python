48901daee35ab7d60453af63f68c0025f4f9aae3
MOHIT-IITP <codingbymohit@gmail.com>
2024-02-22 23:32:57 +0530

    Add sorting method

---
+++ CODE/Sorting/bubble_sort.py
@@ -0,0 +1,21 @@
+# Sorting method 1
+# Bubble sort
+
+
+
+def sortting(lst):
+    swapped=False
+    for i in range(len(lst)-1):
+        for j in range(len(lst)-1):
+            if lst[j]>lst[j+1]:
+                swapped=True
+                lst[j],lst[j+1]=lst[j+1],lst[j]
+        if not swapped:
+            return 
+
+list_input= input("Enter number with space : ")
+lst=list_input.split(",")
+sortting(lst)
+for elem in lst:
+    print(elem)
+    print('\n')
