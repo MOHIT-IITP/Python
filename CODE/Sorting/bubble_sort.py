632829e8db5f8d11d4d7113a33aecddc705d2804
MOHIT-IITP <codingbymohit@gmail.com>
2024-03-01 14:11:24 +0530

    add bubble sort

--- CODE/Sorting/bubble_sort.py
+++ CODE/Sorting/bubble_sort.py
@@ -1,21 +1,12 @@
-# Sorting method 1
-# Bubble sort
-
-
-
-def sortting(lst):
-    swapped=False
-    for i in range(len(lst)-1):
-        for j in range(len(lst)-1):
+def bubblesort(lst):
+    n=len(lst)
+    for i in range(n-1,0,-1):
+        for j in range(i):
             if lst[j]>lst[j+1]:
-                swapped=True
                 lst[j],lst[j+1]=lst[j+1],lst[j]
-        if not swapped:
-            return 
+    return lst
+lst=[43,12,45,6234,76,34,76,23,87,23,99]
 
-list_input= input("Enter number with space : ")
-lst=list_input.split(" ")
-sortting(lst)
-for elem in lst:
-    print(elem)
-    print('\n')
+
+# i call the function with print because in neovim(macos) it works like this 
+print(bubblesort(lst))
