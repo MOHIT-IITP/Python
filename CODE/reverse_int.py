474f8cd443fb8cb943227856413c79aa22ac2590
Mohit Kumar <136831716+MOHIT-IITP@users.noreply.github.com>
2024-03-25 21:30:37 +0530

    Create reverse_int.py

---
+++ CODE/reverse_int.py
@@ -0,0 +1,10 @@
+# the below code will reverse the int in python 
+
+def reverse(number):
+  number=str(number)
+  print(number[::-1])
+
+#giving the input 
+number= 12345
+#calling the function
+reverse(number)
