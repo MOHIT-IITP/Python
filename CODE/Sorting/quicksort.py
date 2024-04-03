92cde9e227b5a20b1d315b0a3340051938fa2133
Mohit Kumar <136831716+MOHIT-IITP@users.noreply.github.com>
2024-04-03 15:15:36 +0530

    Update quicksort.py

--- CODE/Sorting/quicksort.py
+++ CODE/Sorting/quicksort.py
@@ -4,7 +4,7 @@ def partition(arr, start, end):
     pivot = arr[end]
     i = start - 1
     for j in range(start, end):
-        if arr[j] <= pivot:
+        if arr[j] <= pivot:                      # only changed this part to convert this code to descending one 
             i += 1
             arr[i], arr[j] = arr[j], arr[i]
     arr[i + 1], arr[end] = arr[end], arr[i + 1]
