1ab906686e29f5503b6906b1eb96a1ea7b90ca3d
MOHIT-IITP <codingbymohit@gmail.com>
2024-05-07 15:28:44 +0530

    update binary tree

--- CODE/Tree/binary_tree.py
+++ CODE/Tree/binary_tree.py
@@ -6,16 +6,42 @@ class Node:
 class BinaryTree:
     def __init__(self):
         self.root = None
+# creating a inorder traversal in binary tree
+def Inorder(node):
+    if node is None:
+        return
+    Inorder(node.left)
+    print(node.data, end=" ")
+    Inorder(node.right)
+
+# creating a preorder traversal in binary tree
+def preorder(node):
+    if node is None:
+        return
+    print(node.data, end=" ")
+    preorder(node.left)
+    preorder(node.right)
+
+# creating a postorder traversal in binary tree
+def postorder(node):
+    if node is None:
+        return 
+    postorder(node.left)
+    postorder(node.right)
+    print(node.data, end=" ")
 
 bt = BinaryTree()
 bt.root=Node(1)
-bt.left=Node(2)
-bt.right=Node(3)
-bt.left.left=Node(4)
-bt.left.right=Node(5)
-bt.right.left=Node(6)
-bt.right.right=Node(7)
+bt.root.left=Node(2)
+bt.root.right=Node(3)
+bt.root.left.right=Node(5)
+bt.root.left.left=Node(4)
+bt.root.right.left=Node(6)
+bt.root.right.right=Node(7)
 
+
+
+# main thing to keep in mind: 
 #preorder  = root left right
 #postorder = left right root
 #inorder = left root right
